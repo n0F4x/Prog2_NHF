05cd10d5d61f41c2c1a35c965c0732075eb50759
n0F4x <ggabor@gmail.com>
2022-04-10 13:01:09 +0200

    Fixed rotation

--- NHF/Source/Menus/GameMenu/Physics.cpp
+++ NHF/Source/Menus/GameMenu/Physics.cpp
@@ -19,25 +19,28 @@ void Physics::rotate(Direction direction) {
 		_rotationClock.restart();
 	}
 }
-//#include <iostream>
+#include <iostream>
 void Physics::update(PlatformContainer& _platforms) {
 	if (_isRotating) {
 		int time = 0;
-		if ((time = _rotationClock.getElapsedTime().asMilliseconds()) <= _rotationDurationTime) {
+		if ((time = _rotationClock.getElapsedTime().asMilliseconds()) < _rotationDurationTime) {
 			float rotation = 0;
 			if (time < _rotationDurationTime / 2) {
-				rotation = _rotationAcceleration * time * time - _rotationLength;
+				rotation = _rotationAcceleration / 2 * time * time - _rotationAcceleration / 2 * _rotationTime * _rotationTime;
+				std::cout << rotation << " first " << _rotationLength << std::endl;
 			}
 			else {
-				rotation = _rotationAcceleration * (_rotationDurationTime - time) * (_rotationDurationTime - time);
+				rotation = _rotationAcceleration / 2 * (_rotationDurationTime - _rotationTime) * (_rotationDurationTime - _rotationTime) - _rotationAcceleration / 2 * (_rotationDurationTime - time) * (_rotationDurationTime - time);
+				std::cout << rotation << " second" << std::endl;
 			}
+			_rotationTime = time;
 			_rotationLength += rotation;
-			//std::cout << rotation << std::endl;
 			_platforms.rotate(rotation * _direction);
 		}
 		else {
-			//std::cout << _rotationDurationLength - _rotationLength << std::endl;
+			std::cout << _rotationDurationLength - _rotationLength << std::endl;
 			_platforms.rotate((_rotationDurationLength - _rotationLength) * _direction);
+			_rotationTime = 0;
 			_rotationLength = 0;
 			_isRotating = false;
 		}
--- NHF/Source/Menus/GameMenu/Physics.hpp
+++ NHF/Source/Menus/GameMenu/Physics.hpp
@@ -18,12 +18,13 @@ private:
 private:
 	const float _rotationDurationLength = Platform::width;
 	const int _rotationDurationTime = 100;	// in milliseconds
-	const float _rotationAcceleration = 2 * _rotationDurationLength / (_rotationDurationTime * _rotationDurationTime);
+	const float _rotationAcceleration = _rotationDurationLength / (_rotationDurationTime / 2 * _rotationDurationTime / 2);
 
 	bool _isRotating = false;
 	int _direction = 0;
 	sf::Clock _rotationClock;
-	float _rotationLength;
+	int _rotationTime = 0;
+	float _rotationLength = 0;
 public:
 	void rotate(Direction direction);
 
