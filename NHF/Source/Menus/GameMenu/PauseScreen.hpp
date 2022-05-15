bd472f42fe34c4faa9416f0cd6d0b9eba327ffc6
Gabor Galgoczy <ggabor@gmail.com>
2022-05-15 03:58:28 +0200

    commenting

--- NHF/Source/Menus/GameMenu/PauseScreen.hpp
+++ NHF/Source/Menus/GameMenu/PauseScreen.hpp
@@ -4,6 +4,9 @@
 #include "../../GUI/Widgets/Button.hpp"
 
 
+/**
+ * @brief	Class for displaying and managing the pause screen filter inside the game menu
+*/
 class PauseScreen : public MenuItem {
 private:
 	sf::RectangleShape _bg{Window::getSize()};
@@ -15,13 +18,33 @@ private:
 
 	bool _gameOver = false;
 
+	/**
+	 * @brief	Renders filter to target
+	 * @param target	The render target
+	 * @param states	The render states
+	*/
 	void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
 
 public:
+	/**
+	 * @brief	Constructs new PauseScreen 
+	 * @param resumeMenu	Function that resumes the (game) menu
+	 * @param closeMenu		Function that closes the (game) menu
+	 * @param initMenu		Function that initiates the (game) menu
+	*/
 	explicit PauseScreen(const std::function<void()>& resumeMenu, const std::function<void()>& closeMenu, const std::function<void()>& initMenu);
 
+	/**
+	 * @brief	Changes the PauseScreen to reflect an end of game filter
+	*/
 	void gameOver();
-
-	void handleEvent(const sf::Event&) override;
+	/**
+	 * @brief	Handles user input
+	 * @param event	User input
+	*/
+	void handleEvent(const sf::Event& event) override;
+	/**
+	 * @brief	Initiates PauseScreen
+	*/
 	void init() override;
 };
--- NHF/Source/Menus/GameMenu/Platform.hpp
+++ NHF/Source/Menus/GameMenu/Platform.hpp
@@ -5,6 +5,9 @@
 #include "../../Utilities/Math/Angle.hpp"
 
 
+/**
+ * @brief	Class that displays and manages a platform
+*/
 class Platform : public sf::Drawable {
 private:
 	static const float _initInnerRadius;
@@ -17,6 +20,10 @@ private:
 	const PreCalculator& _preCalc;
 
 	float _scalingRatio;
+	/**
+	 * @brief	Setter for _scalingRatio
+	 * @param speed	The amount of times a platform needs to be updated to move by a range of 1 platform
+	*/
 	void setScale(unsigned speed);
 
 	float _innerRadius = _initInnerRadius;
@@ -24,22 +31,70 @@ private:
 	float _rotation = 0_deg;
 	const float _width;
 
+	/**
+	 * @brief	Draws the platform to the render target
+	 * @param target	The render target
+	 * @param target	The render states
+	*/
 	void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
 
 public:
+	/**
+	 * @brief	Setter for _origin
+	 * @param origin	The new origin 
+	*/
 	static void setOrigin(const sf::Vector2f& origin);
 
+	/**
+	 * @brief	Constructs a new Platform
+	 * @param preCalc	Reference to a pre-calculator
+	 * @param rotation	Initial rotation of the platform (in radians)
+	 * @param width		Width of the platform (in radians)
+	 * @param speed		The amount of times a platform needs to be updated to move by a range of 1 platform
+	*/
 	Platform(const PreCalculator& preCalc, float rotation, float width, unsigned speed);
 
+	/**
+	 * @brief	Getter for inner radius
+	 * @return	The inner radius of the platform
+	*/
 	float getInnerRadius() const { return _innerRadius; }
+	/**
+	 * @brief	Getter for outer radius
+	 * @return	The outer radius of the platform
+	*/
 	float getOuterRadius() const { return _outerRadius; }
+	/**
+	 * @brief	Getter for rotation
+	 * @return	The rotation of the platform in radians
+	*/
 	float getRotation() const { return _rotation; }
+	/**
+	 * @brief	Getter for width
+	 * @return	The width of the platform in radians
+	*/
 	float getWidth() const { return _width; }
 
+	/**
+	 * @brief	Check whether a point is on the platform
+	 * @param point	The point in question
+	 * @return	True if the point is on the platform
+	*/
 	bool isInside(const PolarVector& point) const;
 
+	/**
+	 * @brief	Rotates the platform by the amount given as param
+	 * @param angle	The amount that will be added to the platforms rotation (in radians)
+	*/
 	void rotate(float angle);
 
+	/**
+	 * @brief	Updates the platform
+	*/
 	void update();
+	/**
+	 * @brief	Checks whether the platform is still inside the area of the window
+	 * @return	True if the platform is outside
+	*/
 	bool isDead() const;
 };
